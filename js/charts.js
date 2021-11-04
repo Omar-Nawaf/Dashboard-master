73c221b6b463f1bd7174367b8fc07f12e3ac63bc
OmarNawaf <omar.nawaf11@gmail.com>
2021-11-04 11:37:02 +0300

    Added all charts but except pie chart

--- js/charts.js
+++ js/charts.js
@@ -1,12 +1,113 @@
 
 // Line Chart for Orders
-var ordersChart = document.getElementById("myAreaChart");
+
+const ordersChart = document.getElementById("myAreaChart");
+
+yValuesArea = [7000 , 30000, 25000, 22000, 8000, 45000, 43000, 40000, 18000, 27000, 4000, 4000, 7000];
+let dataArea = [];
+for (let i = 0; i < yValuesArea.length; i++) {
+    dataArea[i] = {x: `Aug ${i+1}`, y: yValuesArea[i]};
+}
+new Chart(ordersChart, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: dataArea,
+            fill: true,
+            backgroundColor:['rgba(64, 29, 186, 0.5)'],
+            borderColor: ['rgba(218, 65, 103, 1)'],
+            tension: '0.2',
+            pointBackgroundColor: 'rgba(218, 65, 103, 1)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+            x: {
+                grid: {
+                    display: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
 
 // Bar Chart for Visitor
-var visitorChart = document.getElementById("myBarChart");
+const visitorChart = document.getElementById("myBarChart");
 
+const dataVisitor1 = [{x: 'June', y: 4000}, {x: 'July', y: 4000}, {x: 'August', y: 13000}, {x: 'September', y: 4000}, {x: 'October', y: 4000}, {x: 'November', y: 4000}, {x: 'December', y: 4000}];
+const dataVisitor2 = [{x: 'June', y: 6000}, {x: 'July', y: 6000}, {x: 'August', y: 15000}, {x: 'September', y: 6000}, {x: 'October', y: 8000}, {x: 'November', y: 2000}, {x: 'December', y: 11000}];
+
+new Chart(visitorChart, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Men',
+            data: dataVisitor1,
+            borderColor: 'rgba(64, 29, 186, 1)',
+            backgroundColor: 'rgba(64, 29, 186, 1)'
+        }, {
+            label: 'Women',
+            data: dataVisitor2,
+            borderColor: 'rgba(218, 65, 103, 1)',
+            backgroundColor: 'rgba(218, 65, 103, 1)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+        plugins: {
+            legend: {
+                display: true
+            }
+        }
+    },
+});
 // Liner Chart for Cities
 var citiesChart = document.getElementById("myPieChart");
 
 // Liner Chart for Rat
-var ratChart = document.getElementById("myLinearChart");
+const ratChart = document.getElementById("myLinearChart");
+
+const dataRat = [{x: 'January', y: 1.5}, {x: 'Fabruary', y: 2.5}, {x: 'March', y: 3}, {x: 'April', y: 5}, {x: 'May', y: 4}, {x: 'June', y: 5}];
+
+new Chart(ratChart, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: dataRat,
+            fill: true,
+            backgroundColor:'rgba(64, 29, 186, 0.1)',
+            borderColor: 'rgba(218, 65, 103, 1)',
+            tension: '0.2',
+            pointBackgroundColor: 'rgba(218, 65, 103, 1)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+            x: {
+                grid: {
+                    display: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    },
+});
